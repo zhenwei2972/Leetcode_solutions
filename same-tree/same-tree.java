af60dca7bf494d3caa91f64d98a35b0acc068f2a
zhenwei2972 <zhenwei2972@hotmail.com>
2021-12-01 17:03:21 +0800

    Time: 0 ms (100.00%), Space: 36.1 MB (98.61%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -15,13 +15,28 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //establish base case for recursion
+       if(p==null ^ q ==null) return false;
        if(p==null && q == null) return true;
-       if(p ==null ^ q == null) return false;
-       if(p.val != q.val)
-       {
-           return false;
-       }
-       return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+       
+        // use stack for DFS search 
+        Stack<TreeNode> s1 = new Stack<>();
+        s1.push(p);
+        Stack<TreeNode> s2 = new Stack<>();
+        s2.push(q);
+        while(!s1.isEmpty() && !s2.isEmpty())
+        {
+            TreeNode s1Val = s1.pop();
+            TreeNode s2Val = s2.pop();
+            if(s1Val.val != s2Val.val) return false;
+            if(s1Val.left ==null^ s2Val.left==null ) return false;
+            if(s1Val.right==null ^ s2Val.right ==null) return false;
+            
+            if(s1Val.left !=null) s1.push(s1Val.left);
+            if(s1Val.right !=null) s1.push(s1Val.right);
+            if(s2Val.left !=null) s2.push(s2Val.left);
+            if(s2Val.right !=null) s2.push(s2Val.right);
+            
+        }
+        return true;
     }
 }
